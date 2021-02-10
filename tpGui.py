140162d3044b96623b344f735f699c52d7c47bbc
chabrol <olivier.chabrol@univ-amu.fr>
2021-02-10 15:25:04 +0100

    theo is unleashed

--- tpGui.py
+++ tpGui.py
@@ -12,19 +12,19 @@ widgets_labs = {}
 widgets_entry = {}
 widgets_button = {}
 
-for idi in ids:
-    lab = Label(root, text=idi)
-    widgets_labs[idi] = lab
-    lab.pack()
+for idi in range(0,len(ids)):
+    lab = Label(root, text=ids[idi])
+    widgets_labs[ids[idi]] = lab
+    lab.grid(row=idi,column=0)
 
     var = StringVar()
     entry = Entry(root, text=var)
-    widgets_entry[idi] = entry
-    entry.pack()
+    widgets_entry[ids[idi]] = entry
+    entry.grid(row=idi,column=1)
 
-for idi in bouton:
-    button = Button(root, text = idi)
-    widgets_button[idi] = button
-    button.pack()
+for idi in range(0,len(bouton)):
+    button = Button(root, text = bouton[idi])
+    widgets_button[bouton[idi]] = button
+    button.grid(row=len(ids)+1,column=idi)
 
 root.mainloop()
