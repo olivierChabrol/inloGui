99373c28870318ce2bd030dee4b556bb9f077e6b
chabrol <olivier.chabrol@univ-amu.fr>
2021-02-10 15:29:57 +0100

    Megane Law's

--- tpGui.py
+++ tpGui.py
@@ -12,19 +12,25 @@ widgets_labs = {}
 widgets_entry = {}
 widgets_button = {}
 
-for idi in range(0,len(ids)):
-    lab = Label(root, text=ids[idi])
-    widgets_labs[ids[idi]] = lab
-    lab.grid(row=idi,column=0)
+i, j = 0, 0
+
+for idi in ids:
+    lab = Label(root, text=idi)
+    widgets_labs[idi] = lab
+    lab.grid(row=i,column=0)
 
     var = StringVar()
     entry = Entry(root, text=var)
-    widgets_entry[ids[idi]] = entry
-    entry.grid(row=idi,column=1)
+    widgets_entry[idi] = entry
+    entry.grid(row=i,column=1)
 
-for idi in range(0,len(bouton)):
-    button = Button(root, text = bouton[idi])
-    widgets_button[bouton[idi]] = button
-    button.grid(row=len(ids)+1,column=idi)
+    i += 1
+
+for idi in bouton:
+    button = Button(root, text = idi)
+    widgets_button[idi] = button
+    button.grid(row=i+1,column=j)
+
+    j += 1
 
 root.mainloop()
